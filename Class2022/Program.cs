bdfca6128e0bb4366767ddc3d97c7f691e2e054a
marcio-kendi <iaimeu@hotmail.com>
2022-11-17 21:37:17 +1300

    Changed code to match class

--- Class2022/Program.cs
+++ Class2022/Program.cs
@@ -1,19 +1,93 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
+
+// open chrome browser
 IWebDriver driver = new ChromeDriver();
 driver.Manage().Window.Maximize();
 
-driver.Navigate().GoToUrl("https://www.google.com");
+// launch turnup portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+Thread.Sleep(1000);
 
-IWebElement searchInput = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
-searchInput.SendKeys("Origami");
+// identify username textbox and enter valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
 
-IWebElement searchButton = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]"));
-searchButton.Click();
+// identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
 
-IWebElement visualSearch = driver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[1]/div[1]/div[2]/div/div[2]/input"));
+// click login button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
 
-var equalText = visualSearch.TagName == "Origami" ? "Found Origami Text" : "Failed";
+// check if user has logged in successfully 
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
 
-Console.WriteLine(equalText);
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("Logged in successfully, test passed.");
+}
+else
+{
+    Console.WriteLine("Login failed, test failed.");
+}
+
+
+// navigate to Time and Material
+IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+administrationDropdown.Click();
+Thread.Sleep(500);
+
+IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmOption.Click();
+
+// click on Create New button
+Thread.Sleep(1500);
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+createNewButton.Click();
+Thread.Sleep(500);
+
+// select Time in the typecode dropdown
+IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+typeCodeDropdown.Click();
+
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[2]"));
+timeOption.Click();
+
+// enter code in the code textbox
+IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("November2022");
+
+// enter description in the description textbox
+IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+descriptionTextbox.SendKeys("November2022");
+
+// enter price in price per unit textbox
+IWebElement overlappingTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+overlappingTag.Click();
+
+IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+priceTextbox.SendKeys("12");
+
+// click on save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(2000);
+
+// check if new time record has been created successfully
+IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPageButton.Click();
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if (newCode.Text == "November2022")
+{
+    Console.WriteLine("Time record created successfully.");
+}
+else
+{
+    Console.WriteLine("Time record hasn't been created successfully");
+}
